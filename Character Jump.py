2dfa68d560aa6e7e9a818aa9869d937b773a53db
Liju <86706612+LIJU321@users.noreply.github.com>
2022-05-05 16:43:34 +0530

    Add files via upload

---
+++ Character Jump.py
@@ -0,0 +1,86 @@
+import pygame 
+import sys
+import os
+
+#varaiables:
+fps = 60
+Red = (255,0,0)
+Run = True
+jumping= False
+jump = 20
+velocity = jump
+gravity = 1
+x_axis , y_axis = 150, 275
+
+
+
+#initializing display
+pygame.init()
+
+#setting Display
+SCREEN = pygame.display.set_mode((600,460))
+
+#giving color to the screen
+SCREEN.fill(Red)
+
+#giving title for window
+pygame.display.set_caption("Character_Jump")
+
+#uploading image from dierctory
+backround_img = pygame.image.load("Assets\BG.png")
+standing_img = pygame.image.load("Assets\mega-man.png")
+jumping_img = pygame.image.load("Assets\megaman_jump.png")
+
+#rezise image
+backround_img_rezised = pygame.transform.scale(backround_img,(100,100))
+standing_img_rezised = pygame.transform.scale(standing_img,(50,50))
+jumping_img_rezised = pygame.transform.scale(jumping_img,(60,50))
+
+#creating a rect for character image:
+
+Standing_Rect = standing_img_rezised.get_rect(topright=(x_axis,y_axis))
+
+Jumping_Rect = jumping_img_rezised.get_rect(topright=(x_axis,y_axis))
+
+
+
+
+#function to draw on display:
+def draw_to_screen(Jumping_Rect):
+    SCREEN.fill(Red)
+    SCREEN.blit(backround_img,(0,0))
+    if jumping==False:
+        SCREEN.blit(standing_img_rezised,(x_axis,y_axis))
+    else:
+     SCREEN.blit(jumping_img_rezised,(x_axis,y_axis))
+
+    pygame.display.update()
+    
+
+
+
+# main function game loop:
+while Run:
+    Clocks = pygame.time.Clock()
+    Clocks.tick(fps)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            Run==False
+            sys.exit()
+    keypress = pygame.key.get_pressed()
+    if keypress[pygame.K_ESCAPE]:
+            Run==False
+            sys.exit()
+    if keypress[pygame.K_SPACE]:
+           jumping = True
+
+    if jumping == True:
+        y_axis-=velocity
+        velocity-=gravity
+    if velocity<-jump:
+        jumping=False
+        velocity=jump
+
+
+    draw_to_screen(y_axis)
+
